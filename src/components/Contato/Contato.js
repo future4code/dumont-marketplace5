ef6439f699a14036a1eaabd30a65f512a172ebde
Vinicius Silveira Moraes <vinicius_jag@hotmail.com>
2020-11-05 01:31:45 -0300

    criados componentes basicos e finalizada tela inicial

---
+++ src/components/Contato/Contato.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Contato extends React.Component{
+    render(){
+        return(
+            <div>
+                Contato
+            </div>
+        )
+    }
+}
+
+export default Contato
