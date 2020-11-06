924c31e39c1920c0cf474005acf4fcdc4d5dfbd2
Alexandre <alexandre.yukon@gmail.com>
2020-11-06 15:27:31 -0300

    criacao pagina quem somos

--- src/components/QuemSomos/QuemSomos.js
+++ src/components/QuemSomos/QuemSomos.js
@@ -1,10 +1,29 @@
 import React from 'react'
+import './quemSomos.css'
 
 class QuemSomos extends React.Component{
     render(){
         return(
-            <div>
-                Quem Somos
+            <div id="tamanhoDiv">
+                
+                <div className="divTitulo">
+                <h1>Quem Somos</h1>
+                </div>
+
+                <div>
+                <h2>
+                Qual é o nosso propósito?
+                </h2>
+                </div>
+
+                <div>
+                <p>
+                O Site 4Used Old is Cool aborda temas ecológicos e ligados à sustentabilidade, como reciclagem e reutilização.
+                O site também está atento a formas de transportes mais inteligentes e sustentáveis, assim como ideias de casas modernas e racionais.
+                Nosso foco é tranformar o velho em novo, reutilizável e facilitar a vida de quem está preocupado com o nosso planeta. 
+                </p>
+                </div>
+
             </div>
         )
     }
