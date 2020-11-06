b137155863de14f904093c160f0d66d402bf2654
Isabelar07 <isabelarochas@outlook.com.br>
2020-11-05 22:36:39 -0300

    tentei pegar a imagem e o preço dos produtos na página de compras

--- src/components/AnuncioItem/AnuncioItem.js
+++ src/components/AnuncioItem/AnuncioItem.js
@@ -1,10 +1,38 @@
+import axios from 'axios';
 import React from 'react'
 
 class AnuncioItem extends React.Component{
+    state = {
+        products: []
+    }
+
+
+    componentDidMount = () => {
+        this.pegarImgPreco()
+    }
+
+    pegarImgPreco = () => {
+        axios.get('https://us-central1-labenu-apis.cloudfunctions.net/fourUsedOne/products').then((res) => {
+        this.setState({ products: res.data})
+        }).catch((erro) => {
+            console.log(erro.message)
+        })
+    }
+
+
     render(){
+        const renderizarImgPreco = this.state.products.map((produto) => {
+        return (
+            <div> 
+            <p>{produto.photo}</p>
+            <p>{produto.price}</p>
+            </div>
+        )
+        })
+
         return(
             <div>
-                Anuncios
+                {renderizarImgPreco}
             </div>
         )
     }
